105a0c753e09a4ba066e494d79b41b0cc043f39b
Cody Bouscaren <codybousc@gmail.com>
2015-11-03 09:27:14 -0500

    Add new restaurant function added

--- js/RestController.js
+++ js/RestController.js
@@ -1,11 +1,23 @@
 bestRest.controller('RestController', function RestController($scope) {
-  var restaurants = [
+    $scope.restaurants = [
     {name: "Tbone's Joint", foodtype: "Ribs", priceRange: "moderate", takeOut: "yes", delivery: "no", ratings: "5 stars"},
     {name: "Pedro's Pizzeria", foodtype: "Pizza", priceRange: "expensive", takeOut: "yes", delivery: "yes", ratings: "2 stars"},
     {name: "From Thailand, with love", foodtype: "Thai", priceRange: "inexpensive", takeOut: "yes", delivery: "yes", ratings: "4 stars"}
   ];
+  $scope.addRest = function() {
+    $scope.restaurants.push(
+      {
+        name: $scope.restName,
+        foodtype: $scope.foodtype,
+        priceRange: $scope.priceRange,
+        takeOut: $scope.takeOut,
+        delivery: $scope.delivery,
+        ratings: $scope.ratings
 
-  $scope.goodies = restaurants;
+      });
+    };
+
+  $scope.goodies = $scope.restaurants;
 
 
 });
